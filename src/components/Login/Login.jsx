775a46affd98d62e1de50237aa6f83020b165f6c
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2022-06-17 08:58:16 +0530

    footer

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
-
+import React from "react";
+import "./login.css";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 const Login = () => {
+  const [show, setshow] = useState(false);
+  const [number,setnumber] = useState("");
+  const [take,settake] = useState("")
+  const navigate = useNavigate()
   return (
-    <div>
-      
+    <div className="login">
+      <img
+        className="loginimg"
+        src="https://www.reliancedigital.in/medias/Umbrella-Offer-Desktop-Banner-1200x500-19-04-2022-01.jpg?context=bWFzdGVyfGltYWdlc3wxNTg5MTU2fGltYWdlL2pwZWd8aW1hZ2VzL2hlMy9oODEvOTgxNjE2NTE4NzYxNC5qcGd8OTBhMTdmZGFjOTU5Yjg4MDk1NDNkYjFhODA0MjA4YzgyODc5M2M2ZTBkNTQwZDk5NTI0ODRiMmU3NzRiYTlhMQ"
+        alt=""
+      />
+      <div className="logintext">
+        <div className="loginbtndetail">
+          <h3>Login / Register</h3>
+          <hr />
+          {show === false ? (
+            <div className="inputlogin">
+              <input onChange={(e) => setnumber(e.target.value)} className="logininput" type="number" />
+              <button disabled={number===10} onClick={() => {
+                if(number.length===10){
+                  setshow(!show)
+                }
+                else{
+                  alert("Mobile Number Should have 10 Numbers")
+                }
+                }} className="loginbtn">
+                PROCEED
+              </button>
+            </div>
+          ) : (
+            <div className="otplogin">
+              <h4>OTP Verification</h4>
+              <p className="loginbtnup">Please enter the OTP sent to your mobile number {number}</p>
+              <div className="putotp">
+                <input onChange={(e) => settake(e.target.value)} type="number" placeholder="Enter OTP*" className="inputputlogin" name="" id="" />
+                <p className="resend">Resend OTP</p>
+                <button onClick={() => {
+                      if(take==="878909"){
+                        navigate("/",{replace:true})
+                      }
+                }} className="submitlogin">LOGIN</button>
+                <button onClick={() => setshow(!show)} className="backlogin">
+                  BACK
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -10,50 +10,81 @@ const Cart = () => {
         <div className="itemcount">
           <h3>My Cart(1 items)</h3>
           <h6>shiping to: 400103</h6>
-          
         </div>
         <hr />
         {cartdata.map((e) => (
           <div id="conflex">
-          <div className="mapitem">
-            <div className="imgitem">
-              <img className="itemimg" src={e.img} alt="" />
-              <br />
-              <button>+</button>
-              {e.count}
-              <button>-</button>
+            <div className="mapitem">
+              <div className="imgitem">
+                <img className="itemimg" src={e.img} alt="" />
+                <br />
+                <button>+</button>
+                {e.count}
+                <button>-</button>
+              </div>
+              <div className="itemtitle">
+                <h3 className="top">{e.title}</h3>
+                <h4 id="margin-top">485984989</h4>
+              </div>
+              <div className="itemprice">
+                <h2 className="top">₹{e.price}</h2>
+                <p id="discountcolor" className="topside">
+                  M.R.P <span id="discount">₹{e.Dprice}</span> Inclusive of all
+                  taxes
+                </p>
+                <p id="freeshiping" className="topside">
+                  Free Shipping
+                </p>
+                <h5 id="delivery" className="top">
+                  🚚Standard Delivery: 19 Jun (Sun)-20 Jun (Mon)
+                </h5>
+                <p id="des" className="top">
+                  *Delivery assurance is subject to our delivery locations
+                  staying open as per govt. regulations
+                </p>
+              </div>
             </div>
-            <div className="itemtitle">
-              <h3 className="top">{e.title}</h3>
-              <h4 id="margin-top">485984989</h4>
-            </div>
-            <div className="itemprice">
-              <h2  className="top">₹{e.price}</h2>
-              <p id="discountcolor"  className="topside">
-                M.R.P <span id="discount">₹{e.Dprice}</span> Inclusive of all taxes
-              </p>
-              <p id="freeshiping" className="topside">Free Shipping</p>
-              <h5 id="delivery" className="top">🚚Standard Delivery: 19 Jun (Sun)-20 Jun (Mon)</h5>
-              <p id="des" className="top">*Delivery assurance is subject to our delivery locations staying open as per govt. regulations</p>
+            <div className="removeitem">
+              <button className="btn1r">Remove</button>
+              <button className="btn1r">Move to wishlist</button>
             </div>
           </div>
-          <div className="removeitem">
-            <button className="btn1r">Remove</button>
-            <button className="btn1r">Move to wishlist</button>
-          </div>
-         </div>
         ))}
-       
-      
-      </div> 
+      </div>
       <div className="checkitem">
-        <button>CHECKOUT</button>
+        <button className="checkbtn1">CHECKOUT</button>
         <div className="cartcheck">
-          
+          <div className="cartcheckcoupon">
+            <input
+              className="checkcoupon"
+              type="text"
+              placeholder="Coupon Code"
+            />
+            <button className="applycoupon">APPLY</button>
+            <hr className="hrcheck" />
+            <div className="pricedetail">
+              <h3>PRICE DETAILS</h3>
+              <div className="totalpricecart">
+                <div className="price2item">
+                  <p className="price2color">Price(2 items)</p><h4 className="checkcartprice">3555454</h4>
+                </div>
+                <div className="deliveryitem">
+                  <p className="price2color">Delivery Charges</p>
+                  <h3 className="h4freedelivery">FREE</h3>
+                </div>
+                <hr className="hrcheck2"/>
+                <div className="amttotalcart">
+                  
+                    <h4 className="amt">AMOUNT PAYABLE</h4>
+                    <h3 className="amtcart">452555</h3>
+                 
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
